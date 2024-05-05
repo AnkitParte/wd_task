fcf4b8b329443699f689e4435a58b9fdbec1caa4
AnkitParte <ankitparte80@gmail.com>
2024-05-05 08:33:24 +0530

    improvement in selectbox

--- src/components/JobFilters/temp.jsx
+++ src/components/JobFilters/temp.jsx
@@ -1,23 +1,24 @@
 import * as React from 'react'
 import { useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import OutlinedInput from '@mui/material/OutlinedInput'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select from '@mui/material/Select'
-import Chip from '@mui/material/Chip'
+// import OutlinedInput from '@mui/material/OutlinedInput'
+// import InputLabel from '@mui/material/InputLabel'
+// import MenuItem from '@mui/material/MenuItem'
+// import FormControl from '@mui/material/FormControl'
+// import Select from '@mui/material/Select'
+// import Chip from '@mui/material/Chip'
+import { MenuItem, FormControl, Select, Chip } from '@mui/material'
 
 const ITEM_HEIGHT = 48
 const ITEM_PADDING_TOP = 8
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
-    }
-  }
-}
+// const MenuProps = {
+//   PaperProps: {
+//     style: {
+//       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+//       width: 250
+//     }
+//   }
+// }
 
 const names = [
   'Oliver Hansen',
@@ -58,28 +59,41 @@ export default function MultipleSelectChip() {
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }} size='small'>
-        <InputLabel id='demo-multiple-chip-label'>Chip</InputLabel>
+        {personName.length != 0 && (
+          <div style={{ fontWeight: 'bold', fontSize: '12px' }}>Roles</div>
+        )}
         <Select
-          labelId='demo-multiple-chip-label'
+          //   style={{ marginTop: '10px' }}
+          //   labelId='demo-multiple-chip-label'
           //   id='demo-multiple-chip'
           multiple
+          displayEmpty
           value={personName}
           onChange={handleChange}
-          input={<OutlinedInput id='select-multiple-chip' label='Chip' />}
-          renderValue={(selected) => (
-            <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-              {selected.map((value) => (
-                <Chip
-                  key={value}
-                  label={value}
-                  onDelete={() => console.log('delete')}
-                  size='small'
-                  style={{ borderRadius: '4px' }}
-                />
-              ))}
-            </Box>
-          )}
-          MenuProps={MenuProps}
+          renderValue={(selected) => {
+            if (selected.length === 0) {
+              return (
+                <span
+                  style={{ color: 'grey', fontWeight: 400, fontSize: '12px' }}
+                >
+                  Roles
+                </span>
+              )
+            }
+            return (
+              <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={value}
+                    onDelete={() => console.log('delete')}
+                    size='small'
+                    style={{ borderRadius: '4px' }}
+                  />
+                ))}
+              </Box>
+            )
+          }}
           inputProps={{ 'aria-label': 'Without label' }}
         >
           {names.map((name) => (
